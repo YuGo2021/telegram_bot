2d240242a1e16a654184bc94594526e95ff8a0a2
Yuriy Golubev <88974156+YuGo2021@users.noreply.github.com>
2023-01-16 22:34:47 +0300

    Куча изменений: 1) токен в отдельный файл 2) режим тест и отладка 3) исправлена ошибка с падением бота 4) сообщение всем пользователям бота

--- screen_bot.py
+++ screen_bot.py
@@ -1,5 +1,5 @@
 import telebot
-import os
+
 from os import getenv
 from selenium import webdriver
 from telebot import types
@@ -9,17 +9,40 @@ import time
 import openpyxl
 from datetime import datetime
 import os
+from winreg import *
+import time
+from dotenv import load_dotenv
+
+time.sleep(10)
+
+
+# Путь в реестре
+key_my = OpenKey(HKEY_CURRENT_USER, r'SOFTWARE\Microsoft\Windows\CurrentVersion\Run', 0, KEY_ALL_ACCESS)
+# Установить программу "screen_bot" в автозагрузку
+#dir = input("Скопируйте и вставьте путь к файлу бота: ")
+#dir_bot = f"{dir}"
+#SetValueEx(key_my, 'mynotepad', 0, REG_SZ, dir_bot)
+#dir_bot = os.path.abspath('screen_bot.exe')
+dir_bot = r"C:\Users\yuri.golubev\Desktop\telegram_bot\screen_bot.exe"
+print(dir_bot)
+SetValueEx(key_my, 'screen_bot', 0, REG_SZ, dir_bot)
+# Закрыть реестр
+CloseKey(key_my)
+
+
 
 homepath = os.getenv('USERPROFILE')
 #print(homepath)  # C:\Users\MyUser
 
-chrome_path = os.path.normpath(homepath + '/AppData/Local/Google/Chrome/User Data/Defaul')
+chrome_path = os.path.normpath(homepath + '/AppData/Local/Google/Chrome/User Data/Default')
 #print(chrome_path)  # C:\Users\MyUser\Music
 
 
 
-bot_token = getenv("BOT_TOKEN")
-#bot_token = "########################################"
+#bot_token = getenv("BOT_TOKEN")
+load_dotenv("bot_api.env")
+bot_token = getenv("TOKEN_BOT")
+
 if not bot_token:
     exit("Error: no token provided")
 
@@ -44,16 +67,88 @@ options1.add_argument(f"user-data-dir={chrome_path}")
 
 
 admin = [112533702]
-users = {}
+
 
 #users.append(admin[0])
+# def read_users():
+#     wb_users = openpyxl.load_workbook("Пользователи.xlsx")
+#     sheet_users = wb_users.active
+#     users1 = {}
+#     for i in range(1, sheet_users.max_row + 1):
+#         users1.update({(sheet_users.cell(row=i, column=1)).value: {"name": (sheet_users.cell(row=i, column=2)).value, "status": (sheet_users.cell(row=i, column=3)).value}})
+#
+#     users1.update({admin[0]:{"name": "Golubev", "status": 1}})
+#     wb_users.save("Пользователи.xlsx")
+#     return users1
+def users_update(user_dict, user_id, users_name, user_status):
+    wb_users = openpyxl.load_workbook("Пользователи.xlsx")
+    sheet_users = wb_users.active
+    #users2 = {}
+    # for i in range(1, sheet_users.max_row + 1):
+    #     users2.update({(sheet_users.cell(row=i, column=1)).value: {"name": (sheet_users.cell(row=i, column=2)).value,
+    #                                                               "status": (sheet_users.cell(row=i, column=3)).value}})
+
+    #users.update({admin[0]: {"name": "Golubev", "status": 1}})
+    #print(users)
+    # temp_name = "no_fucking_name"
+    # if user_id in users:
+    #     temp_name = users[user_id]["name"]
+    # if users_name != "noname":
+    #     users.update({user_id: {"name": users_name, "status": user_status}})
+    # else:
+    #     #users.update({user_id: {"status": user_status}})
+    #     users.update({user_id: {"name": temp_name, "status": user_status}})
+    user_dict.update({user_id: {"name": users_name, "status": user_status}})
+    for user in user_dict:
+        print(user, user_dict[user]["name"], user_dict[user]["status"])
+    line = 0
+    for user in user_dict:
+        line += 1
+        sheet_users.cell(row=line, column=1).value = user
+        sheet_users.cell(row=line, column=2).value = user_dict[user]["name"]
+        sheet_users.cell(row=line, column=3).value = user_dict[user]["status"]
+    wb_users.save("Пользователи.xlsx")
+
+def add_user(user_dict, user_id, users_name, user_status):
+    wb_users = openpyxl.load_workbook("Пользователи.xlsx")
+    sheet_users = wb_users.active
+    user_dict.update({user_id: {"name": users_name, "status": user_status}})
+    for user in user_dict:
+        print(user, user_dict[user]["name"], user_dict[user]["status"])
+    line = 0
+    for user in user_dict:
+        line += 1
+        sheet_users.cell(row=line, column=1).value = user
+        sheet_users.cell(row=line, column=2).value = user_dict[user]["name"]
+        sheet_users.cell(row=line, column=3).value = user_dict[user]["status"]
+    wb_users.save("Пользователи.xlsx")
+
+def set_user_status(user_id, status):
+    wb_users = openpyxl.load_workbook("Пользователи.xlsx")
+    sheet_users = wb_users.active
+    users3 = {}
+    for i in range(1, sheet_users.max_row + 1):
+        users3.update({(sheet_users.cell(row=i, column=1)).value: {"name": (sheet_users.cell(row=i, column=2)).value,
+                                                                  "status": (sheet_users.cell(row=i, column=3)).value}})
+    for user in users3:
+        if user == user_id:
+            users3[user]["status"] = status
+    for user in users3:
+        print(user, users3[user]["name"], users3[user]["status"])
+    wb_users.save("Пользователи.xlsx")
+
+
 wb_users = openpyxl.load_workbook("Пользователи.xlsx")
 sheet_users = wb_users.active
-for i in range(2, sheet_users.max_row + 1):
-    users.update({(sheet_users.cell(row=i, column=1)).value: {"name": (sheet_users.cell(row=i, column=2)).value, "status": (sheet_users.cell(row=i, column=3)).value}})
+global user1
+users1 = {}
+for i in range(1, sheet_users.max_row + 1):
+    users1.update({(sheet_users.cell(row=i, column=1)).value: {"name": (sheet_users.cell(row=i, column=2)).value, "status": (sheet_users.cell(row=i, column=3)).value}})
 
-users.update({admin[0]:{"name": "Golubev", "status": 1}})
+users1.update({admin[0]:{"name": "Golubev", "status": 1}})
+wb_users.save("Пользователи.xlsx")
 
+#print(users)
 # only used for console output now
 def listener(messages):
     """
@@ -64,7 +159,7 @@ def listener(messages):
             # print the sent message to the console
             print(str(m.chat.first_name) + " [" + str(m.chat.id) + "]: " + m.text)
 
-@bot.message_handler(func=lambda message: message.chat.id not in users)
+@bot.message_handler(func=lambda message: message.chat.id not in users1)
 def some(message):
     # wb_users = openpyxl.load_workbook("Пользователи.xlsx")
     # sheet_users = wb_users.active
@@ -72,17 +167,17 @@ def some(message):
     #     if sheet_users.cell(row=i, column=1) not in users:
     #         users.append(sheet_users.cell(row=i, column=1))
 
-
-    users.update({message.chat.id: {"name": message.chat.first_name, "status": 2}})
-    for user in users:
-        print(user, users[user]["name"], users[user]["status"])
-    line = 0
-    for user in users:
-        line += 1
-        sheet_users.cell(row=line, column=1).value = user
-        sheet_users.cell(row=line, column=2).value = users[user]["name"]
-        sheet_users.cell(row=line, column=3).value = users[user]["status"]
-    wb_users.save("Пользователи.xlsx")
+    users_update(users1, int(message.chat.id), message.chat.first_name, 2)
+    # users.update({message.chat.id: {"name": message.chat.first_name, "status": 2}})
+    # for user in users:
+    #     print(user, users[user]["name"], users[user]["status"])
+    # line = 0
+    # for user in users:
+    #     line += 1
+    #     sheet_users.cell(row=line, column=1).value = user
+    #     sheet_users.cell(row=line, column=2).value = users[user]["name"]
+    #     sheet_users.cell(row=line, column=3).value = users[user]["status"]
+    # wb_users.save("Пользователи.xlsx")
     bot.send_message(message.chat.id, 'Это закртый бот. Запросите доступ у администратора')
     bot.send_message(admin[0], 'Есть новые пользователи для подключения. Нажмите /start')
     #
@@ -101,7 +196,8 @@ def start(m, res=False):
     # for i in range(1, sheet_users.max_row + 1):
     #     if sheet_users.cell(row=i, column=1) not in users:
     #         users.append(sheet_users.cell(row=i, column=1))
-
+    #users = read_users()
+    users = users1
     if m.chat.id == admin[0]:
         print(users)
         for user in users:
@@ -112,8 +208,8 @@ def start(m, res=False):
                 # markup.row(item_yes, item_no)
                 # bot.send_message(m.chat.id, f'{users[user]["name"]} [{user}] запрашивает доступ к боту', reply_markup=markup)
                 keyboard = telebot.types.InlineKeyboardMarkup()
-                button1 = telebot.types.InlineKeyboardButton(text="Предоставить доступ", callback_data=f"AddUser:{user}")
-                button2 = telebot.types.InlineKeyboardButton(text="Отказать в доуступе", callback_data=f"Decline:{user}")
+                button1 = telebot.types.InlineKeyboardButton(text="Предоставить доступ", callback_data=f"AddUser:{user}/{users[user]['name']}")
+                button2 = telebot.types.InlineKeyboardButton(text="Отказать в доступе", callback_data=f"Decline:{user}/{users[user]['name']}")
                 keyboard.row(button1, button2)
                 bot.send_message(m.chat.id, f'{users[user]["name"]} [{user}] запрашивает доступ к боту', reply_markup=keyboard)
     if m.chat.id in users and users[m.chat.id]["status"] == 1:
@@ -125,12 +221,14 @@ def start(m, res=False):
         item2 = types.KeyboardButton("Врачи")
         item3 = types.KeyboardButton("ВО")
         item4 = types.KeyboardButton("ОО")
+        item5 = types.KeyboardButton("Дежурный")
+        item6 = types.KeyboardButton("B2B")
         # markup.add(item1)
         # markup.add(item2)
         # markup.add(item3)
         # markup.add(item4)
-        markup1.row(item1, item2)
-        markup1.row(item3, item4)
+        markup1.row(item1, item5, item2)
+        markup1.row(item3, item4, item6)
         bot.send_message(m.chat.id, 'Для получения нужного скриншота нажми на кнопку (время загрузки около 10 сек)',  reply_markup=markup1)
     elif m.chat.id in users and users[m.chat.id]["status"] == 2:
         bot.send_message(m.chat.id, 'Ваш запрос на доступ к боту на рассмотрении. Для ускорения обратитесь к администратору')
@@ -144,7 +242,10 @@ def handle_text(message):
     # for i in range(1, sheet_users.max_row + 1):
     #     if sheet_users.cell(row=i, column=1) not in users:
     #         users.append(sheet_users.cell(row=i, column=1))
-    if message.chat.id in users and users[message.chat.id]["status"] == 1:
+    #users4 = read_users()
+    users4 = users1
+    print(f"Запрос от {message.chat.first_name} в {datetime.now()}")
+    if message.chat.id in users4 and users4[message.chat.id]["status"] == 1:
 
         # отправляем скрин борда по запросу
         if message.text.strip() == 'Общий борд КЦ' :
@@ -235,9 +336,49 @@ def handle_text(message):
                 driver.save_screenshot(photo_path4)
                 send_time4 = datetime.now()
             bot.send_photo(uid, photo=open(photo_path4, 'rb'))
+        elif message.text.strip() == 'Дежурный':
+            uid = message.chat.id
+            driver = webdriver.Chrome(chrome_options=options)
+            driver.set_window_size(1920, 1200)
+            now = datetime.now()
+            if 'send_time5' not in globals():
+                global send_time5
+                send_time5 = datetime(2017, 7, 18, 4, 52, 33, 51204)
+            delta = now - send_time5
+            if delta.seconds > 60:
+                global photo_path5
+                photo_path5 = "Image\\" + str(random.randint(10000000, 99999999)) + '.png'
+
+                driver.get("https://grafana2.gemotest.ru:3000/d/_xv1gmGVk/dezhurnyi?orgId=1&refresh=30s")
+                # driver.get("https://yandex.ru/")
+                time.sleep(10)
+                driver.save_screenshot(photo_path5)
+                send_time5 = datetime.now()
+            bot.send_photo(uid, photo=open(photo_path5, 'rb'))
             #driver.quit()
-    if message.chat.id == admin[0]:
-        if message.text.strip() == 'отладка':
+        elif message.text.strip() == 'B2B':
+            uid = message.chat.id
+            driver = webdriver.Chrome(chrome_options=options)
+            driver.set_window_size(1920, 1200)
+            now = datetime.now()
+            if 'send_time6' not in globals():
+                global send_time6
+                send_time6 = datetime(2017, 7, 18, 4, 52, 33, 51204)
+            delta = now - send_time6
+            if delta.seconds > 60:
+                global photo_path6
+                photo_path6 = "Image\\" + str(random.randint(10000000, 99999999)) + '.png'
+
+                driver.get("https://grafana2.gemotest.ru:3000/d/Dlb_TeI4k/b2b?orgId=1")
+                # driver.get("https://yandex.ru/")
+                time.sleep(10)
+                driver.save_screenshot(photo_path6)
+                send_time6 = datetime.now()
+            bot.send_photo(uid, photo=open(photo_path6, 'rb'))
+            # driver.quit()
+    #    if message.chat.id == admin[0]:
+    #       if message.text.strip() == 'отладка':
+        elif message.text.strip() == 'отладка':
             uid = message.chat.id
             photo_path = "Image\\" + str(random.randint(10000000, 99999999)) + '.png'
             driver = webdriver.Chrome(chrome_options=options1)
@@ -247,6 +388,20 @@ def handle_text(message):
             time.sleep(30)
             driver.save_screenshot(photo_path)
             bot.send_photo(uid, photo=open(photo_path, 'rb'))
+        elif message.text.strip() == 'тест':
+            uid = message.chat.id
+            photo_path = "Image\\" + str(random.randint(10000000, 99999999)) + '.png'
+            driver = webdriver.Chrome(chrome_options=options)
+            driver.set_window_size(1920, 1080)
+            #driver.get("https://grafana2.gemotest.ru:3000/d/i_tq1wY7z/glavnaia-2?orgId=1&refresh=5m")
+            driver.get("https://yandex.ru/")
+            time.sleep(10)
+            driver.save_screenshot(photo_path)
+            bot.send_photo(uid, photo=open(photo_path, 'rb'))
+        elif message.text.strip()[:4] == 'All:':
+            for user in users4:
+                if users4[user]["status"] == 1:
+                    bot.send_message(user, f"{message.text.strip()[5:]}")
         # if message.text.strip()[:7] == 'AddUser:':
         #     print(message.text.strip[8:])
         #     if message.chat.id == admin[0]:
@@ -295,39 +450,52 @@ def handle_text(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_function1(callback_obj):
-    if callback_obj.data[:8] == "AddUser:":
-        for user in users:
-            if user == callback_obj.data[8:]  and users[user]["status"] == 1:
+
+    #users5 = read_users()
+    command = callback_obj.data.partition("/")
+    if command[0][:8] == "AddUser:":
+        for user in users1:
+            if user == int(command[0][8:]) and users1[user]["status"] == 1:
                 #bot.send_message(message.chat.id, 'Пользовтель уже добавлен')
                 bot.send_message(callback_obj.from_user.id, f"Пользовтель {callback_obj.data[8:]} уже добавлен")
-
                 break
+
         else:
-            users[int(callback_obj.data[8:])]["status"] = 1
-            for user in users:
-                print(user, users[user]["name"], users[user]["status"])
-            line = 0
-            for i in users:
-                line += 1
-                sheet_users.cell(row=line, column=1).value = i
-                sheet_users.cell(row=line, column=2).value = users[i]["name"]
-                sheet_users.cell(row=line, column=3).value = users[i]["status"]
-            wb_users.save("Пользователи.xlsx")
+            #set_user_status(int(command[0][8:]), 1)
+            #users_update(int(command[0][8:]), command[2], 1)
+            add_user(users1, int(command[0][8:]), command[2], 1)
+            #users_update(int(callback_obj.data[8:]), "noname", 1)
+
+            #users_update(callback_obj.data[8:], "noname", 1)
+            # users[int(callback_obj.data[8:])]["status"] = 1
+            # for user in users:
+            #     print(user, users[user]["name"], users[user]["status"])
+            # line = 0
+            # for i in users:
+            #     line += 1
+            #     sheet_users.cell(row=line, column=1).value = i
+            #     sheet_users.cell(row=line, column=2).value = users[i]["name"]
+            #     sheet_users.cell(row=line, column=3).value = users[i]["status"]
+            # wb_users.save("Пользователи.xlsx")
             #bot.send_message(message.chat.id, 'Пользовтель добавлен')
             bot.send_message(callback_obj.from_user.id, f"Пользовтель {callback_obj.data[8:]} добавлен")
-            bot.send_message(int(callback_obj.data[8:]), 'Вам предоставлен доступ к боту. Нажмите /start')
-
+            bot.send_message(int(command[0][8:]), 'Вам предоставлен доступ к боту. Нажмите /start')
+            #break
     elif callback_obj.data[:8] == "Decline:":
-        users[int(callback_obj.data[8:])]["status"] = 0
-        # print(users, users["status"])
-        line = 0
-        for i in users:
-            line += 1
-            sheet_users.cell(row=line, column=1).value = i
-            sheet_users.cell(row=line, column=2).value = users[i]["name"]
-            sheet_users.cell(row=line, column=3).value = users[i]["status"]
-        wb_users.save("Пользователи.xlsx")
-        bot.send_message(int(callback_obj.data[8:]), 'Вам отказано в достпуе к боту')
+        #set_user_status(int(command[0][8:]), 0)
+        #users_update(int(command[0][8:]), command[2], 0)
+        add_user(users1, int(command[0][8:]), command[2], 0)
+        #users_update(int(callback_obj.data[8:]), "noname", 0)
+        # users[int(callback_obj.data[8:])]["status"] = 0
+        # # print(users, users["status"])
+        # line = 0
+        # for i in users:
+        #     line += 1
+        #     sheet_users.cell(row=line, column=1).value = i
+        #     sheet_users.cell(row=line, column=2).value = users[i]["name"]
+        #     sheet_users.cell(row=line, column=3).value = users[i]["status"]
+        # wb_users.save("Пользователи.xlsx")
+        bot.send_message(int(command[0][8:]), 'Вам отказано в достпуе к боту')
 
         bot.send_message(callback_obj.from_user.id, f"Пользователь {callback_obj.data[8:]} заблокирован")
 
@@ -372,7 +540,15 @@ def callback_function1(callback_obj):
 #         bot.send_message(message.text.strip[8:], 'Вам отказано в достпуе к боту')
 
 # Запускаем бота
-bot.polling(none_stop=True, interval=0)
+while True:
+    try:
+        bot.polling(none_stop=True, interval=0)
+        break
+    except Exception as e:
+        print(f"Ошибка {e} ({datetime.now()})")
+        bot.stop_polling()
+        time.sleep(20)
+
 
 
 # отправка картинки пользователю
